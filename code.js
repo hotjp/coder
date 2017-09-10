b6360e9b94fafe701a0fd154657d9233f04b4910
hotjp <hotjp@126.com>
2017-09-10 15:05:29 +0800

    ?

--- code.js
+++ code.js
@@ -1,12 +1,23 @@
-var re = /x/;
-var i = 0;
-console.log(re);
+var openedTimes = 0;
+// window.re = /x/;
+// re.toString = function() {
+//   ++i;
+//   console.clear();
+//   return '\u6765\u627e\u5230\u6211';
+// };
+// console.log(re);
 
-re.toString = function() {
-  return (++i);
-};
-if (i) {
-  document.write('');
+// if (!i) {
+//   document.write('');
+// }
+
+function consoleTool(name, callback) {
+  var tool = window[name] = /x/;
+  tool.toString = callback;
+  console.log(tool);
 }
 
-Function("".replace(/.{8}/g,function(u){return String.fromCharCode(parseInt(u.replace(/\u200c/g,1).replace(/\u200d/g,0),2))}))();
+consoleTool('clear',function(){
+  console.clear();
+  return 'Console was cleared';
+});
